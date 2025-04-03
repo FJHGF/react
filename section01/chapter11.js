009fb2a533a4195890539417fc7dc865bceab4f4
FJHGF <fjhgf@naver.com>
2025-04-03 19:49:06 +0900

    0403 merge

---
+++ section01/chapter11.js
@@ -0,0 +1,18 @@
+// 중복 코드 > 함수
+
+// function greeting() {
+//   console.log("안녕하세요.");
+// }
+
+// console.log("호출전");
+// greeting();
+// console.log("호출후");
+
+function getArea(width, height) {
+  // let width = 10;
+  // let height = 20;
+  let area = width * height;
+  console.log(area);
+}
+
+getArea(10, 30);
