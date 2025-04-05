f90eaf51fd8ac3fd91c71871685ac0bcb47b4a15
FJHGF <fjhgf@naver.com>
2025-04-05 19:33:37 +0900

    0405_5.6

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,14 +2,26 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Button from "./components/Button";
 
 // 루트 컴퍼넌트
 function App() {
+  const buttonProps = {
+    text: "cafe",
+    color: "blue",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button text={"mail"} color={"red"} a={1} b={2} />
+      <Button {...buttonProps} />
+      <Button text={"blog"}>
+        <Header />
+        <div>자식 요소</div>
+      </Button>
     </>
   );
 }
---
+++ section05/src/components/Button.jsx
@@ -0,0 +1,24 @@
+const Button = ({ children, text, color = "black" }) => {
+  // console.log(text);
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    // SyntheticBaseEvent : 통일된 규칙
+    console.log(text);
+  };
+  return (
+    <button
+      // onClick={() => {
+      //   console.log(text);
+      // }}
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
+      {text}-{color.toUpperCase()}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
